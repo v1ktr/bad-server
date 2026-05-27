2d1b83f1740bd46d566f76e9eafe6114b66cccd3
Виктор Сипин <viktor@Air-Viktor.lan>
2026-05-27 17:54:45 +0300

    v9 fixing audit, search

--- backend/src/controllers/customers.ts
+++ backend/src/controllers/customers.ts
@@ -95,6 +95,7 @@ export const getCustomers = async (
             const escapeRegex = (value: string) => value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
             const safeSearch = escapeRegex(search as string)
             const searchRegex = new RegExp(safeSearch, 'i')
+            //const searchRegex = new RegExp(search as string, 'i')
             const orders = await Order.find(
                 {
                     $or: [{ deliveryAddress: searchRegex }],
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
+        loadPaths: [resolve('./src/scss')],
         additionalData: `
-          @use "./src/scss/variables" as *;
-          @use "./src/scss/mixins";
+          @use "variables" as *;
+          @use "mixins";
         `,
       },
-
     }
   },
 
