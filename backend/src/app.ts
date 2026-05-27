93098f90b8ddeac2ab9f8da9ad8e5cde3554d800
Виктор Сипин <viktor@Air-Viktor.lan>
2026-05-27 15:44:22 +0300

    v5.1 fixing ratelimit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -34,7 +34,7 @@ app.use(
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     //max: 10,
-    limit: 5,
+    limit: 50,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
