bc490d9473e33f60f9df9f51281026fb4d993c37
Виктор Сипин <viktor@Air-Viktor.lan>
2026-05-27 17:15:40 +0300

    v8 fixing search

--- backend/src/routes/customers.ts
+++ backend/src/routes/customers.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Joi } from 'celebrate'
 import {
     deleteCustomer,
     getCustomerById,
@@ -11,7 +12,17 @@ import { Role } from '../models/user'
 const customerRouter = Router()
 
 customerRouter.get('/', auth, roleGuardMiddleware(Role.Admin), getCustomers)
-customerRouter.get('/:id', auth, roleGuardMiddleware(Role.Admin), getCustomerById)
+customerRouter.get(
+    '/:id',
+    celebrate({
+        params: Joi.object({
+            id: Joi.string().hex().length(24).required(),
+        }),
+    }),
+    auth,
+    roleGuardMiddleware(Role.Admin),
+    getCustomerById
+)
 customerRouter.patch('/:id', auth, roleGuardMiddleware(Role.Admin), updateCustomer)
 customerRouter.delete('/:id', auth, roleGuardMiddleware(Role.Admin), deleteCustomer)
 
